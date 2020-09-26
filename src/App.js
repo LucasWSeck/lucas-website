fe85bd9ec2ff0964b8391886e9354d55026bda75
Jonas Laux <jonas@anyline.io>
2020-09-26 14:32:47 +0200

    finished page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import "./App.css";
+import {
+  GithubOutlined,
+  InstagramOutlined,
+  TwitterOutlined,
+} from "@ant-design/icons";
+import { Button } from "antd";
+import styled from "styled-components";
 
-function App() {
+import lucas from "./lucas.png";
+
+const IconContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 130px;
+`;
+
+const Image = styled.img`
+  margin-bottom: 120px;
+  width: 400px;
+  height: auto;
+`;
+
+const App = () => {
   return (
     <div className="App">
+      <h1>Lucas Seck</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Image src={lucas} className="App-logo" alt="logo" />
+        <p>Yoo Leute. Meine Page!</p>
+        <IconContainer>
+          <Button
+            type="circle"
+            icon={<InstagramOutlined />}
+            href="https://www.instagram.com/lucasseck_"
+            target="_blank"
+          />
+          <Button
+            type="circle"
+            icon={<TwitterOutlined />}
+            href="https://twitter.com/Lucas12413672"
+            target="_blank"
+          />
+          <Button
+            type="circle"
+            icon={<GithubOutlined />}
+            href="https://github.com/LucasWSeck"
+            target="_blank"
+          />
+        </IconContainer>
       </header>
     </div>
   );
-}
+};
 
 export default App;
